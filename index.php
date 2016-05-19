9ef0bb5cc48cb621ac9d8a0f69c780ee2b7fa5b3
Cazei <Carl@carica.com>
2016-05-19 13:09:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+
+	<head>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Eprojet test </title>
+		
+		
+		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+		
+		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
+		
+		        <link rel="stylesheet" href="css/stylesheet.css">
+	</head>
+	
+	<body>
+		<div class="pure-g">
+			<div id="header" class="pure-u-1">
+				
+				
+				<p>header</p>
+				
+				
+			</div>
+			<div id="content" class="pure-u-1 pure-u-sm-3-4">
+			<p>Thirds</p>
+			
+			</div>
+			
+			<div id="feed" class="pure-u-1 pure-u-sm-1-4">
+				
+			
+			<p>Thirds</p>
+			
+		</div>
+		</div>
+	
+	
+	
+
+	</body>
+</html>
